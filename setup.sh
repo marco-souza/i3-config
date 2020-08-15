6535280af4aff9f7220ec2e40338b8c887aa5048
Marco Souza <ma.souza.junior@gmail.com>
2020-08-15 14:27:18 -0300

    fea: add kde system setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-# /bin/bash
+#! /bin/bash
 
-# TODO: Check/install settings
-yay -S nitrogen gnome-keyring compton
+# install i3 (arch)
+[[ ! -e /usr/bin/i3 ]] && sudo pacman -Syu nitrogen gnome-keyring compton i3
+
+# copy initializer option
+filename=
+sudo cp ./plasma-i3.desktop /usr/share/xsessions/plasma-i3.desktop
+cp config $HOME/.config/i3/config
+i3 restart
