c168479aa9bff859090477d806d2de66361a5f89
Marco Souza <ma.souza.junior@gmail.com>
2020-08-16 10:11:20 -0300

    enh: update config

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 [[ ! -e /usr/bin/i3 ]] && sudo pacman -Syu nitrogen gnome-keyring compton i3
 
 # copy initializer option
-filename=
 sudo cp ./plasma-i3.desktop /usr/share/xsessions/plasma-i3.desktop
+[[ ! -d $HOME/.config/i3/ ]] && mkdir -p $HOME/.config/i3/
 cp config $HOME/.config/i3/config
-i3 restart
